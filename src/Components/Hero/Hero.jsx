fdbdf539c295ff9fe8cf9742c6029215faecf36b
MiguelAOCosta <miguelangelocosta1996@gmail.com>
2021-10-31 16:36:52 +0000

    Changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
     <HeroContainer>
       <FirstSection>
         <HeroTitle>Um passo mais próximo dos teus sonhos</HeroTitle>
-        <HeroBtn> Corte</HeroBtn>
+        <HeroBtn>Agendar Corte</HeroBtn>
       </FirstSection>
       <DesktopSection>
         <DesktopImg src={bgBarber} />
