c44e5e26fda53341f6f570d8e185737d8d0c4ff5
MiguelAOCosta <miguelangelocosta1996@gmail.com>
2021-11-01 22:28:09 +0000

    New

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
+import Drop from "./Components/DropDownNav/Drop";
+import Info from "./Components/Info/Info";
+import Nav from "./Components/Navigation/Nav";
 import GlobalStyle from "./GlobalStyles";
-import Navbar from "./Components/Navigation/NavBar";
-import Hero from "./Components/Hero/Hero";
-import About from "./Components/About/About";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(0);
+  const toogle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Navbar />
-      <Hero />
-      <About />
+      <Info />
+      <Nav isOpen={isOpen} toogle={toogle} />
+      <Drop isOpen={isOpen} toogle={toogle} />
     </>
   );
 }
---
+++ src/Components/DropDownNav/DropStyles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const DropContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #adadad;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  z-index: 99;
+  position: absolute;
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+  transition: all 0.2s ease-in-out;
+`;
+
+export const NavBar = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  list-style: none;
+  padding-top: 30px;
+`;
+
+export const NavLink = styled.li`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  padding: 1rem 0;
+  transition: all 0.2s ease-in-out;
+  position: relative;
+
+  &:hover {
+    background: rgba(151, 79, 49, 0.5);
+  }
+`;
