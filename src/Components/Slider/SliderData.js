73a881859928c53e017db4b04f9608c1fd3eec6a
MiguelAOCosta <miguelangelocosta1996@gmail.com>
2021-11-07 01:46:16 +0000

    Silder Done

--- src/Components/Slider/SliderData.js
+++ src/Components/Slider/SliderData.js
@@ -1,8 +1,10 @@
-import img1 from "../../Images/index.jpg";
+import img1 from "../../Images/img1.jpg";
+import img2 from "../../Images/img2.jpg";
+import img3 from "../../Images/img3.jpg";
 
 export const ImgSlider = [
   {
-    img: img1,
+    img: img2,
     title1: "Não é apenas um corte de cabelo, é uma experiência",
     title2: "Ser barbeiro é cuidar das pessoas.",
   },
@@ -12,7 +14,7 @@ export const ImgSlider = [
     title2: "Onde os homens querem estar no seu melhor.",
   },
   {
-    img: img1,
+    img: img3,
     title1: "Não é apenas um corte de cabelo, é uma experiência",
     title2: "Os penteados realçam o seu sorriso.",
   },
