14d682c2ccc8befe8a69218e22ec42cdb8ae87da
MiguelAOCosta <miguelangelocosta1996@gmail.com>
2021-11-10 23:22:26 +0000

    Services Section

--- src/Components/OurServices/Services.jsx
+++ src/Components/OurServices/Services.jsx
@@ -7,7 +7,17 @@ import {
   ImgTitle,
   SectionParagraph,
 } from "../../GlobalStyles";
-import { ServicesSection } from "./ServicesStyles";
+import {
+  ServicesSection,
+  ImgSection,
+  ServicesContainer,
+  ServicesContent,
+  ServiceInfo,
+  ServiceTitle,
+  ServiceParagraph,
+  ServiceImg,
+  ImgService,
+} from "./ServicesStyles";
 import LogoImg from "../../Images/title.svg";
 import ServicesImg from "../../Images/services.png";
 import Tesoura from "../../Images/tesoura.svg";
@@ -15,7 +25,11 @@ import Tesoura from "../../Images/tesoura.svg";
 const Services = () => {
   return (
     <Section
-      style={{ backgroundColor: "#f9f9f9", borderTop: "2px solid #f1f1f1" }}
+      style={{
+        backgroundColor: "#f9f9f9",
+        borderTop: "2px solid #f1f1f1",
+        padding: "6rem 2rem 0 2rem",
+      }}
     >
       <SectionTitle>
         <Title>Serviços</Title>
@@ -32,11 +46,63 @@ const Services = () => {
       </SectionTitle>
 
       <ServicesSection>
-        <h1>Teste</h1>
-        <h1>Teste</h1>
-        <img src={ServicesImg} style={{ width: "40%" }} />
-        <h1>Teste</h1>
-        <h1>Teste</h1>
+        <ServicesContainer>
+          <ServicesContent>
+            <ServiceInfo style={{ textAlign: "right" }}>
+              <ServiceTitle>Corte de cabelo</ServiceTitle>
+              <ServiceParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+                architecto omnis accusamus ipsam! Vel, assumenda!
+              </ServiceParagraph>
+            </ServiceInfo>
+            <ServiceImg style={{ marginLeft: "1rem" }}>
+              <ImgService src={Tesoura} />
+            </ServiceImg>
+          </ServicesContent>
+
+          <ServicesContent>
+            <ServiceInfo style={{ textAlign: "right" }}>
+              <ServiceTitle>Lavagem de cabelo</ServiceTitle>
+              <ServiceParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+                architecto omnis accusamus ipsam! Vel, assumenda!
+              </ServiceParagraph>
+            </ServiceInfo>
+            <ServiceImg style={{ marginLeft: "1rem" }}>
+              <ImgService src={Tesoura} />
+            </ServiceImg>
+          </ServicesContent>
+        </ServicesContainer>
+
+        <ImgSection src={ServicesImg} />
+
+        <ServicesContainer>
+          <ServicesContent>
+            <ServiceImg style={{ marginRight: "1rem" }}>
+              <ImgService src={Tesoura} />
+            </ServiceImg>
+            <ServiceInfo style={{ textAlign: "left" }}>
+              <ServiceTitle>Hair Styling</ServiceTitle>
+              <ServiceParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+                architecto omnis accusamus ipsam! Vel, assumenda!
+              </ServiceParagraph>
+            </ServiceInfo>
+          </ServicesContent>
+
+          <ServicesContent>
+            <ServiceImg style={{ marginRight: "1rem" }}>
+              <ImgService src={Tesoura} />
+            </ServiceImg>
+            <ServiceInfo style={{ textAlign: "left" }}>
+              <ServiceTitle>Corte de Barba</ServiceTitle>
+              <ServiceParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+                architecto omnis accusamus ipsam! Vel, assumenda!
+              </ServiceParagraph>
+            </ServiceInfo>
+          </ServicesContent>
+        </ServicesContainer>
       </ServicesSection>
     </Section>
   );
