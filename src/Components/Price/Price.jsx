d78dc63e6a11d0feaedc82b812ecbe390f39c743
MiguelAOCosta <miguelangelocosta1996@gmail.com>
2021-11-29 00:48:22 +0000

    Prices-2

--- src/Components/Price/Price.jsx
+++ src/Components/Price/Price.jsx
@@ -7,7 +7,16 @@ import {
   ImgTitle,
 } from "../../GlobalStyles";
 import LogoImg from "../../Images/title.svg";
-import { PriceSection } from "./PriceStyles";
+import {
+  PriceSection,
+  PriceContainer,
+  PriceType,
+  TypeContainer,
+  Type,
+  TypeInfo,
+  PriceInfo,
+  Description,
+} from "./PriceStyles";
 
 const Price = () => {
   return (
@@ -15,7 +24,6 @@ const Price = () => {
       style={{
         backgroundColor: "#f9f9f9",
         borderTop: "2px solid #f1f1f1",
-        padding: "6rem 2rem 0 2rem",
       }}
     >
       <SectionTitle style={{ marginBottom: "6rem" }}>
@@ -26,7 +34,77 @@ const Price = () => {
       </SectionTitle>
 
       <PriceSection>
-        <h1>Preços Aqui!</h1>
+        <PriceContainer>
+          <PriceType>Cabelo</PriceType>
+          <TypeContainer>
+            <Type>
+              <TypeInfo>Corte</TypeInfo>
+              <PriceInfo>10€</PriceInfo>
+            </Type>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias, quas!
+            </Description>
+          </TypeContainer>
+          <TypeContainer>
+            <Type>
+              <TypeInfo>Penteado</TypeInfo>
+              <PriceInfo>12€</PriceInfo>
+            </Type>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias, quas!
+            </Description>
+          </TypeContainer>
+        </PriceContainer>
+
+        <PriceContainer>
+          <PriceType>Barba</PriceType>
+          <TypeContainer>
+            <Type>
+              <TypeInfo>Barber total</TypeInfo>
+              <PriceInfo>5€</PriceInfo>
+            </Type>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias, quas!
+            </Description>
+          </TypeContainer>
+          <TypeContainer>
+            <Type>
+              <TypeInfo>Barbear</TypeInfo>
+              <PriceInfo>8€</PriceInfo>
+            </Type>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias, quas!
+            </Description>
+          </TypeContainer>
+          <TypeContainer>
+            <Type>
+              <TypeInfo>Barbear soave</TypeInfo>
+              <PriceInfo>10€</PriceInfo>
+            </Type>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias, quas!
+            </Description>
+          </TypeContainer>
+        </PriceContainer>
+
+        <PriceContainer>
+          <PriceType>Tratamentos</PriceType>
+          <TypeContainer>
+            <Type>
+              <TypeInfo>Limpeza facial</TypeInfo>
+              <PriceInfo>15€</PriceInfo>
+            </Type>
+            <Description>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias, quas!
+            </Description>
+          </TypeContainer>
+        </PriceContainer>
       </PriceSection>
     </Section>
   );
